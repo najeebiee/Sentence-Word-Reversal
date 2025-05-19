821c5dc01922b21882c55c3759de7841e7f297c2
najeebiee <nmapantas_240000002090@uic.edu.ph>
2025-05-19 16:10:25 +0800

    Add files via upload

---
+++ Mapantas/src/main/java/main/Mapantas_TLA3.java
@@ -0,0 +1,45 @@
+
+package main;
+
+import java.util.*;
+
+public class Mapantas_TLA3 {
+
+    public static void main(String[] args) {
+       
+        System.out.println("Sentence Word Reversal");
+        System.out.println("Enter your words (q to Quit)\n");
+        
+        Stack<String> sentence = new Stack<>();
+        Scanner s = new Scanner(System.in);
+        boolean run = true;
+        
+        while (run) {
+            System.out.print("word : ");
+            String word = s.nextLine();
+            
+            if (!word.equalsIgnoreCase("q")) {
+                sentence.push(word);
+            } else {
+                run = false;
+            }
+            
+        }
+        
+        if (!sentence.isEmpty() && sentence.size() > 1) {
+            for (String words : sentence) {
+                System.out.print(words + " ");
+            }
+
+            System.out.print("\nReversed : ");
+
+            while (!sentence.isEmpty()) {
+                String popped = sentence.pop();
+                System.out.print(popped + " ");
+            }
+            
+        }
+        
+    }
+    
+}
